22a2171ab7ff5950db44933494e09e070549bfb7
nithishnithish18 <nithishprabakaran09@gmail.com>
2022-01-21 16:35:02 +0530

    updated driver program

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ from write import to_files
 
 
 def main():
-    name = os.environ.get("NAME")
-    env = os.environ.get("ENVIRON")
-    src_dir = os.environ.get("SRC_DIR")
-    src_file_pattern = f'{os.environ.get("SRC_FILE_PATTERN")}-*'
-    src_file_format = os.environ.get("SRC_FILE_FORMAT")
-    spark = get_spark_session(env, name)
-    df = from_files(spark, src_dir, src_file_pattern, src_file_format)
-    tranasformedDF = transform(df)
-    tgt_dir = os.environ.get("TGT_DIR")
-    tgt_file_format = "parquet"
-    to_files(tranasformedDF,tgt_dir, tgt_file_format)
+    env = os.environ.get('ENVIRON')
+    src_dir = os.environ.get('SRC_DIR')
+    file_pattern = f"{os.environ.get('SRC_FILE_PATTERN')}-*"
+    src_file_format = os.environ.get('SRC_FILE_FORMAT')
+    tgt_dir = os.environ.get('TGT_DIR')
+    tgt_file_format = os.environ.get('TGT_FILE_FORMAT')
+    spark = get_spark_session(env, 'GitHub Activity - Reading and Writing Data')
+    df = from_files(spark, src_dir, file_pattern, src_file_format)
+    df_transformed = transform(df)
+    to_files(df_transformed, tgt_dir, tgt_file_format)
 
 
 if __name__ == '__main__':
